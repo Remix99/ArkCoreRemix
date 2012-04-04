8113da5c4326a28294f810ccf3782eba32f43a3a
Remix99 <remixcata@live.com>
2012-04-04 02:32:23 -0500

    compile fix typo

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -976,7 +976,7 @@ void Spell::SpellDamageSchoolDmg(SpellEffIndex effIndex)
                                 unitTarget->RemoveAuraFromStack(spellId);
                             damage *= doses;
                             //damage += int32(((Player*)m_caster)->GetTotalAttackPowerValue(BASE_ATTACK) * 0.09f * doses);
-                              damage += int32(((Player*)m_caster)->GetTotalAttackPowerValue(BASE_ATTACK) * 0.09f * combo)
+                              damage += int32(((Player*)m_caster)->GetTotalAttackPowerValue(BASE_ATTACK) * 0.09f * combo);
                         }
                         // Eviscerate and Envenom Bonus Damage (item set effect)
                         if (m_caster->HasAura(37169)) damage +=
