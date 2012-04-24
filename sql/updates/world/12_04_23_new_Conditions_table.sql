072d263abbf70e164e1591b1b80550136b151a48
Remix99 <remixcata@live.com>
2012-04-24 02:44:09 -0500

    trash

--- sql/updates/world/12_04_23_new_Conditions_table.sql
+++ sql/updates/world/12_04_23_new_Conditions_table.sql
@@ -1,24 +1,3 @@
-/*
-SQLyog Community v9.63 
-MySQL - 5.5.22 : Database - arkdb
-*********************************************************************
-*/
-
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`arkdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `arkdb`;
-
-/*Table structure for table `conditions` */
-
 DROP TABLE IF EXISTS `conditions`;
 
 CREATE TABLE `conditions` (
