a967ba601b7085da5cd755f8e9dca2cb3dfc1eb9
unknown <Administrator@eagle355.startdedicated.com>
2012-07-08 21:57:52 -0500

    Core/Vehicles: Rename vehicle_accessory to vehicle_template_accessory Core/Spells: Add new SpellInfo class to preparation of new spellsystem

--- src/server/game/Entities/Vehicle/Vehicle.h
+++ src/server/game/Entities/Vehicle/Vehicle.h
@@ -135,6 +135,7 @@ public:
         return m_vehicleInfo;
     }
 
+    uint32 const& GetCreatureEntry() const { return m_creatureEntry; }
     bool HasEmptySeat (int8 seatId) const;
     Unit *GetPassenger (int8 seatId) const;
     int8 GetNextEmptySeat (int8 seatId, bool next, bool byAura = false) const;
@@ -163,6 +164,7 @@ protected:
     VehicleEntry const *m_vehicleInfo;
     uint32 m_usableSeatNum;          // Number of seats that match VehicleSeatEntry::UsableByPlayer, used for proper display flags
     uint32 m_bonusHP;
+    uint32 m_creatureEntry;
 
     SeatMap::iterator GetSeatIteratorForPassenger (Unit* passenger);
     void InitMovementInfoForBase ();
