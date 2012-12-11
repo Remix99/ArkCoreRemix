d22743b494cd765228ec42ef36a4aa0daadaf641
unknown <Administrator@eagle355.startdedicated.com>
2012-12-11 01:11:35 -0600

    Merge branch 'master' of github.com:Arkania/ArkCORE

--- sql/updates/world/12_12_07_00_gameobject_scripts.sql
+++
@@ -1,5 +0,0 @@
--- Fix Portal Uldum
--- By Mikadmin Fopr Arkania
-
-UPDATE `gameobject_scripts` SET `id` = 4017959 WHERE `entry` = 50010;
-UPDATE `gameobject_scripts` SET `id` = 4018087 WHERE `entry` = 50011;
