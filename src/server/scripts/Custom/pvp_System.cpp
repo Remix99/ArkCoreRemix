8a21522dc9867856c0486026d77a2e109e192a9c
unknown <Administrator@eagle355.startdedicated.com>
2013-01-02 12:11:51 -0600

    rank buffs

--- src/server/scripts/Custom/pvp_System.cpp
+++ src/server/scripts/Custom/pvp_System.cpp
@@ -113,105 +113,105 @@ void OnPVPKill(Player *pKiller, Player *pVictim)
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(1) - Beware of |cFFa2cc0f%s|r the Criminal who destroyed |cFFff0000%s|r and is warming up with a 2 person killing spree. Rising to the top!", pKiller->GetName(), pVictim->GetName(), KillStreaks[1]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        pKiller->CastSpell(pKiller,24378,true);
+        pKiller->CastSpell(pKiller,65858,true);
         break;
 
         case KillerStreak20:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(2) - Avoid |cFFa2cc0f%s|r the Insane who dismembered |cFFff0000%s|r and is on fire with a 3 person killing spree. Inspired by death..", pKiller->GetName(), pVictim->GetName(), KillStreaks[2]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-       // pKiller->CastSpell(pKiller,70571,true);
+       // pKiller->CastSpell(pKiller,65874,true);
         break;
 
         case KillerStreak30:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(3) - Watch out! |cFFa2cc0f%s|r the Maniac has slain |cFFff0000%s|r and is on a crazed 4 person killing spree. Full of comtempt!", pKiller->GetName(), pVictim->GetName(), KillStreaks[3]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,69861,true);
+        //pKiller->CastSpell(pKiller,67260,true);
         break;
         
         case KillerStreak40:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(4) - Caution! |cFFa2cc0f%s|r the Serial Killer has massacred |cFFff0000%s|r and is on a 5 person killing spree. Focused on the path.", pKiller->GetName(), pVictim->GetName(), KillStreaks[4]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,47335,true);
+        //pKiller->CastSpell(pKiller,67257,true);
         break;
 
         case KillerStreak50:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(5) - Fate steps aside as |cFFa2cc0f%s|r the Unmerciful has relinquished |cFFff0000%s|r to the heavens and is on a 6 person killing spree. Demonic intentions", pKiller->GetName(), pVictim->GetName(), KillStreaks[5]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        pKiller->CastSpell(pKiller,60342,true);
+        pKiller->CastSpell(pKiller,67259,true);
         break;
         
         case KillerStreak60:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(6) - Attention! |cFFa2cc0f%s|r the Ender has absolved |cFFff0000%s|r and is on a 7 person killing spree. Halfway to the highest of Ken", pKiller->GetName(), pVictim->GetName(), KillStreaks[6]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,62398,true);
+        //pKiller->CastSpell(pKiller,67256,true);
         break;
         
         case KillerStreak70:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(7) - Be wary! |cFFa2cc0f%s|r the Gravedigger has buried |cFFff0000%s|r and is raging with a 8 person killing spree. Working the late night shift!", pKiller->GetName(), pVictim->GetName(), KillStreaks[7]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,51929,true);
+        //pKiller->CastSpell(pKiller,67261,true);
         break;
 
         case KillerStreak80:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(8) - Like clockwork, |cFFa2cc0f%s|r the Mastermind has tactfully removed |cFFff0000%s|r from battle and is on fire with a 9 person killing spree. Motivated by the one.", pKiller->GetName(), pVictim->GetName(), KillStreaks[8]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,63660,true);
+        //pKiller->CastSpell(pKiller,67258,true);
         break;
 
         case KillerStreak90:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(9) - Concealed in the Shadows, |cFFa2cc0f%s|r the Assassin has slain |cFFff0000%s|r and is on a 10 person killing spree. Seeking revenge for the past.", pKiller->GetName(), pVictim->GetName(), KillStreaks[9]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        pKiller->CastSpell(pKiller,43776,true);
+        pKiller->CastSpell(pKiller,80747,true);
         break;
 
         case KillerStreak100:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(10) - Tossing the carcass aside, |cFFa2cc0f%s|r the Bloodletter has devoured |cFFff0000%s|r and is on a 11 person killing spree. Hell walks among the living.", pKiller->GetName(), pVictim->GetName(), KillStreaks[10]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        pKiller->CastSpell(pKiller,41969,true);
+        pKiller->CastSpell(pKiller,95537,true);
         break;
 
         case KillerStreak110:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(11) - Cower before |cFFa2cc0f%s|r the Unstoppable who has slain |cFFff0000%s|r and is on a rampaging 12 person killing spree. Second in command to only one!", pKiller->GetName(), pVictim->GetName(), KillStreaks[11]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,19883,true);
+        //pKiller->CastSpell(pKiller,24378,true);
         break;
         
         case KillerStreak120:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(12) - Fear |cFFa2cc0f%s|r the Conqueror; who has annihilated yet another victim, |cFFff0000%s|r, and is on a 13 person killing spree. UnMatched by all, can no one stop them?", pKiller->GetName(), pVictim->GetName(), KillStreaks[12]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,72726,true);
+        //pKiller->CastSpell(pKiller,24378,true);
         break;
         
         case KillerStreak130:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(13) - Fear |cFFa2cc0f%s|r the Conqueror; who has annihilated yet another victim, |cFFff0000%s|r, and is on a 14 person killing spree. UnMatched by all, can no one stop them?", pKiller->GetName(), pVictim->GetName(), KillStreaks[12]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[1]);
-        //pKiller->CastSpell(pKiller,11009,true);
+        //pKiller->CastSpell(pKiller,24378,true);
         break;
         
         case KillerStreak140:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(14) - Fear |cFFa2cc0f%s|r the Conqueror; who has annihilated yet another victim, |cFFff0000%s|r, and is on a 15 person killing spree. UnMatched by all, can no one stop them?", pKiller->GetName(), pVictim->GetName(), KillStreaks[12]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[2]);
-        //pKiller->CastSpell(pKiller,42828,true);
+        //pKiller->CastSpell(pKiller,24378,true);
         break;
 
         case KillerStreak150:
         sprintf(msg, "|cFFff8400[Kill Streak]:|r -RANK-(15) - The Reaper |cFFa2cc0f%s|r ; Has claimed another soul for the harvest, |cFFff0000%s|r, and is on a 20 person killing spree. Who will be their next victim?!", pKiller->GetName(), pVictim->GetName(), KillStreaks[12]);
         sWorld->SendServerMessage(SERVER_MSG_STRING, msg);
         pKiller->AddItem(ItemReward, AmountOfRewardsOnKillStreak[2]);
-        pKiller->CastSpell(pKiller,42828,true);
+        pKiller->CastSpell(pKiller,24378,true);
         break;
         
         
