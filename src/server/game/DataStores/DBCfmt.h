bd050c2386fdc6fca82a098d8d6e5850073b9c5b
Remix99 <remixcata@live.com>
2013-01-13 19:35:17 -0800

    Core/DBC: Correct dbc data for Holidaysfmt, thanks to @Ladel

--- src/server/game/DataStores/DBCfmt.h
+++ src/server/game/DataStores/DBCfmt.h
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2005 - 2012 MaNGOS <http://www.getmangos.com/>
+ * Copyright (C) 2005 - 2013 MaNGOS <http://www.getmangos.com/>
  *
- * Copyright (C) 2008 - 2012 Trinity <http://www.trinitycore.org/>
+ * Copyright (C) 2008 - 2013 Trinity <http://www.trinitycore.org/>
  *
- * Copyright (C) 2010 - 2012 ProjectSkyfire <http://www.projectskyfire.org/>
+ * Copyright (C) 2010 - 2013 ProjectSkyfire <http://www.projectskyfire.org/>
  *
- * Copyright (C) 2011 - 2012 ArkCORE <http://www.arkania.net/>
+ * Copyright (C) 2011 - 2013 ArkCORE <http://www.arkania.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -74,7 +74,7 @@ const char GtOCTRegenHPfmt[] = "xf";
 const char GtRegenHPPerSptfmt[] = "xf";
 const char GtRegenMPPerSptfmt[] = "xf";
 const char GtSpellScalingfmt[] = "nf";
-const char Holidaysfmt[] = "nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const char Holidaysfmt[] = "niiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiixxsiix";
 const char ItemArmorQualityfmt[] = "nfffffffi";
 const char ItemArmorShieldfmt[] = "nifffffff";
 const char ItemArmorTotalfmt[] = "niffff";
