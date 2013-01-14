a57ba605d566ec6a9ed3bb5aab88911277004fab
Remix99 <remixcata@live.com>
2013-01-13 19:33:24 -0800

    Core/Spells: Empowered Shadow, thanks to Menur

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2005 - 2012 MaNGOS <http://www.getmangos.com/>
+ * Copyright (C) 2005 - 2013 MaNGOS <http://www.getmangos.com/>
  *
- * Copyright (C) 2008 - 2012 Trinity <http://www.trinitycore.org/>
+ * Copyright (C) 2008 - 2013 Trinity <http://www.trinitycore.org/>
  *
- * Copyright (C) 2010 - 2012 ArkCORE <http://www.arkania.net/>
+ * Copyright (C) 2010 - 2013 ArkCORE <http://www.arkania.net/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -742,12 +742,13 @@ void Spell::SpellDamageSchoolDmg (SpellEffIndex effIndex)
                     uint32 pct = stack * 10;
 
                     // Mastery
-                    if (m_caster->HasAuraType(SPELL_AURA_MASTERY))
+                    if (m_caster->HasAuraType(SPELL_AURA_MASTERY)){
                         if (m_caster->ToPlayer()->GetTalentBranchSpec(m_caster->ToPlayer()->GetActiveSpec()) == BS_PRIEST_SHADOW)
                             pct += 1.5f * m_caster->ToPlayer()->GetMasteryPoints();
-
+				 }
                     AddPctN(damage, pct);
                     m_caster->RemoveAurasDueToSpell(77487);
+					m_caster->CastSpell(m_caster, 95799, true);//Give "Empowered Shadow" - ID:95799
                 }
 
                 //Mind Melt Aura remove
